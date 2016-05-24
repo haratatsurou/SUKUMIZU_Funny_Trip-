ff5b412d83b5dcbd5fb8736976b664b8ab7d8d12
KessaPassa <k.someya.gyao@gmail.com>
2016-05-24 23:51:08 +0900

    移動関連つくりました

---
+++ Assets/Script/Moving.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moving : MonoBehaviour
+{
+    public float moveValue = 15f; //移動量
+    public float jumpPower = 2.8f; //ジャンプ力
+    bool isGrounded = false; //地面に接触しているのか判定
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        //ライトキーで右に移動
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(new Vector2(moveValue * Time.deltaTime, 0f));
+        //レフトキーで左に移動
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(new Vector2(-moveValue * Time.deltaTime, 0f));
+        
+        //空中でジャンプできないように制限
+        if (!isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpPower * 1000f));
+            isGrounded = true;
+        }
+    }
+
+
+    //なにかに接触したらもう一度ジャンプできるようにする
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        isGrounded = false;
+    }
+}
