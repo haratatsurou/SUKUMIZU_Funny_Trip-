f96244cbbd92781ccc6b85c5ad48c96b22b3acc1
KessaPassa <k.someya.gyao@gmail.com>
2016-05-25 16:10:52 +0900

    移動関連 spriteとカメラ移動修正

--- Assets/Script/Moving.cs
+++ Assets/Script/Moving.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Moving : MonoBehaviour
 {
+    public float cameraY = 13f;
+    public float cameraZ = -50f;
     public float moveValue = 15f; //移動量
     public float jumpPower = 2.8f; //ジャンプ力
     bool isGrounded = false; //地面に接触しているのか判定
@@ -16,25 +18,30 @@ public class Moving : MonoBehaviour
 
     void Update()
     {
+        Vector3 nowPos = Camera.main.transform.position;
+
         //ライトキーで右に移動
         if (Input.GetKey(KeyCode.RightArrow))
-            transform.Translate(new Vector2(moveValue * Time.deltaTime, 0f));
+            transform.Translate(new Vector2(moveValue * Time.deltaTime, 0f)); //プレイヤーを右に
         //レフトキーで左に移動
         else if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Translate(new Vector2(-moveValue * Time.deltaTime, 0f));
+            transform.Translate(new Vector2(-moveValue * Time.deltaTime, 0f)); //プレイヤーを左に
         
         //空中でジャンプできないように制限
-        if (!isGrounded && Input.GetKeyDown(KeyCode.Space))
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpPower * 1000f));
-            isGrounded = true;
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpPower * 1000f)); //上にジャンプ
+            isGrounded = false;
         }
+
+        nowPos = new Vector3(transform.position.x, cameraY, cameraZ);
+        Camera.main.transform.position = nowPos;
     }
 
 
     //なにかに接触したらもう一度ジャンプできるようにする
     void OnCollisionEnter2D(Collision2D coll)
     {
-        isGrounded = false;
+        isGrounded = true;
     }
 }
