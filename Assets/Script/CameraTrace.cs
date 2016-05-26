888039db9b5c516f504618292b9f4e733ecdbf9d
KessaPassa <k.someya.gyao@gmail.com>
2016-05-26 19:35:08 +0900

    移動モーション完成

--- Assets/Script/CameraTrace.cs
+++ Assets/Script/CameraTrace.cs
@@ -6,14 +6,11 @@ public class CameraTrace : MonoBehaviour
     public float cameraY = 13f; //カメラのy軸(画面の上下を調整)
     public float cameraZ = -50f; //カメラのz軸(画面の前後を調整)
     private GameObject player;
-    private float startTime;
-    private float journeyLength;
 
 
     void Start()
     {
         player = GameObject.FindWithTag("Player"); //プレイヤーを取得
-        startTime = Time.time;
     }
 
     
@@ -23,11 +20,7 @@ public class CameraTrace : MonoBehaviour
         Vector3 pastPos = new Vector3(transform.position.x, cameraY, cameraZ);
 
         nowPos = new Vector3(player.transform.position.x, cameraY, cameraZ); //x軸だけプレイヤーと同じ移動量。y軸とz軸はお好みで
-        journeyLength = Vector3.Distance(nowPos, pastPos);
-        float distCovered = Time.time - startTime;
-        float fracJourney = distCovered / journeyLength;
-        print(fracJourney);
         //transform.position = nowPos; //カメラとプレイヤーの移動量を同期
-        transform.position = Vector3.Lerp(pastPos, nowPos, fracJourney);
+        transform.position = Vector3.Lerp(pastPos, nowPos, Time.deltaTime);
     }
 }
