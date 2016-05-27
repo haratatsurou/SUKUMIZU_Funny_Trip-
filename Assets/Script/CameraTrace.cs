bc685ad403c957fd278178cbeef5811454273d95
KessaPassa <k.someya.gyao@gmail.com>
2016-05-27 22:52:55 +0900

    カメラ制御完成

--- Assets/Script/CameraTrace.cs
+++ Assets/Script/CameraTrace.cs
@@ -7,6 +7,7 @@ public class CameraTrace : MonoBehaviour
     public float cameraY = 13f; //カメラのy軸(画面の上下を調整)
     public float cameraZ = -50f; //カメラのz軸(画面の前後を調整)
     private GameObject player;
+    Vector3 nowPos;
 
 
     void Start()
@@ -18,12 +19,12 @@ public class CameraTrace : MonoBehaviour
     void Update()
     {
         //プレイヤーがデストロイされているならばカメラは動かさない
-        if(player != null)
-        {
-            Vector3 nowPos = transform.position; //カメラの位置情報を毎回取り出す
-            Vector3 pastPos = new Vector3(transform.position.x, cameraY, cameraZ);
-            nowPos = new Vector3(player.transform.position.x, cameraY, cameraZ); //x軸だけプレイヤーと同じ移動量。y軸とz軸はお好みで                                                                  //transform.position = nowPos; //カメラとプレイヤーの移動量を同期
-            transform.position = Vector3.Lerp(pastPos, nowPos, Time.deltaTime);
-        }
+        if (player != null)
+            nowPos = new Vector3(player.transform.position.x, cameraY, cameraZ); //x軸だけプレイヤーと同じ移動量。y軸とz軸はお好みで 
+    }
+
+    void LateUpdate()
+    {
+        transform.position = nowPos; //カメラとプレイヤーの移動量を同期
     }
 }
