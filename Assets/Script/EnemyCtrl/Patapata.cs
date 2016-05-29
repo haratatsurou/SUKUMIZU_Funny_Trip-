02807b9fbaed0dbd1058ee82c570b975cc214d40
KessaPassa <k.someya.gyao@gmail.com>
2016-05-30 04:25:16 +0900

    敵の実装完成

---
+++ Assets/Script/EnemyCtrl/Patapata.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Patapata : MonoBehaviour
+{
+    private Vector3 startPos; //敵の初期位置
+    public float speed = 5f; //移動速度
+    public float moveRange = 20f; //移動範囲
+    private float moveValue; //移動量を入れておく箱
+    private bool isWayY = false; //falseだと下に、trueだと上に動く
+
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+
+    void Update()
+    {
+        //上に移動
+        if (isWayY)
+        {
+            moveValue += Time.deltaTime * speed; //徐々に値を加算
+            //限界まで移動したら下に反転
+            if (moveValue >= startPos.y + moveRange)
+                isWayY = false;
+        }
+        //下に移動
+        else
+        {
+            moveValue -= Time.deltaTime * speed; //徐々に値を減算
+            //限界まで移動したら上に反転
+            if (moveValue <= startPos.y + moveRange * -1f)
+                isWayY = true;
+        }
+
+        transform.position = new Vector2(startPos.x, startPos.y + moveValue); //x軸は固定。y軸を毎回更新して移動させる
+    }
+}
