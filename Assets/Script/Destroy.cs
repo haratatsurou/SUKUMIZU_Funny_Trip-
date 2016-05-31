3653da502e2fb3a1841bb7fa26a4fcbc93974a51
KessaPassa <k.someya.gyao@gmail.com>
2016-05-31 13:53:38 +0900

    細かいところ修正

--- Assets/Script/Destroy.cs
+++ Assets/Script/Destroy.cs
@@ -18,12 +18,13 @@ public class Destroy : MonoBehaviour
     }
 
 
+    //敵に当たるか、下に落ちるかすると死亡
     public void OnTriggerEnter2D(Collider2D coll)
     {
         if (coll.gameObject.tag == "Player")
         {
             Destroy(coll.gameObject);
-            SceneManager.LoadScene("stage01");
+            SceneManager.LoadScene("stage01"); //リスタート
         }
     }
 }
--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -7,6 +7,7 @@ public class PlayerMove : MonoBehaviour
     public float jumpPower = 3f; //ジャンプ力
     private bool isGrounded = false; //地面に接触しているのか判定
     static public int witchWay = 1; //向いている方向。左が0, 右が1
+    private Vector2 startScale;
 
     public Sprite jumpimage,walkimage;
 
@@ -17,6 +18,7 @@ public class PlayerMove : MonoBehaviour
 
     void Start()
     {
+        startScale = transform.localScale; //大きさを取得
         playerrigidbody = GetComponent<Rigidbody2D>( );
         anime = GetComponent<Animator>( );
         anime.enabled = false;
@@ -26,38 +28,28 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
-       
-        //ライトキーで右に移動
-        if ( Input.GetKey(KeyCode.RightArrow) ) {
-            transform.Translate(new Vector2(moveValue * Time.deltaTime , 0f)); //プレイヤーを右に
-            if ( isGrounded ) {
-                anime.enabled = true;
-            }
-        }
-        //レフトキーで左に移動
-        else if ( Input.GetKey(KeyCode.LeftArrow) ) {
-            transform.Translate(new Vector2(-moveValue * Time.deltaTime , 0f)); //プレイヤーを左に
-
-            if ( isGrounded ) {
-                anime.enabled = true;
-            }
-        }
-
         //isGrounded = Physics2D.Linecast (transform.position, transform.position - transform.up * 0.05f, groundLayer);
 
         //ライトキーで右に移動
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(new Vector2(moveValue * Time.deltaTime, 0f)); //プレイヤーを右に
+            transform.localScale = startScale; //画像を右に向ける
             witchWay = 1;
+
+            if (isGrounded)
+                anime.enabled = true;
         }
         //レフトキーで左に移動
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(new Vector2(-moveValue * Time.deltaTime, 0f)); //プレイヤーを左に
+            transform.localScale = new Vector2(-startScale.x, startScale.y); //画像を左に向ける
             witchWay = 0;
-        }
 
+            if (isGrounded)
+                anime.enabled = true;
+        }
         
         //空中でジャンプできないように制限
         if (isGrounded && Input.GetKey(KeyCode.Space))
